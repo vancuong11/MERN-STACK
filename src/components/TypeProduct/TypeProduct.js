5d76d9dbd0548b626e2e4da1d1a8da72cb967341
vancuong11 <cuongg121101@gmail.com>
2023-07-25 13:21:57 +0700

    #5 Cách phân chia thành các component nhỏ

---
+++ src/components/TypeProduct/TypeProduct.js
@@ -0,0 +1,5 @@
+function TypeProduct() {
+    return <div>Type Product</div>;
+}
+
+export default TypeProduct;
--- src/page/HomePage/HomePage.js
+++ src/page/HomePage/HomePage.js
@@ -1,5 +1,21 @@
+import TypeProduct from '../../components/TypeProduct/TypeProduct';
+import './HomePage.scss';
 function HomePage() {
-    return <div>Home Page</div>;
+    const arr = ['TV', 'Tủ Lạnh', 'Laptop', 'Điện Thoại'];
+    return (
+        <div className="container">
+            <div className="wrapper-homepage">
+                {arr.map((index, item) => {
+                    return (
+                        <div className="type-product">
+                            <TypeProduct name={item} key={index} />
+                        </div>
+                    );
+                })}
+            </div>
+            Home Page
+        </div>
+    );
 }
 
 export default HomePage;
