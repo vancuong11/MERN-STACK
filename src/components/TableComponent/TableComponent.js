d33758b4dfb5c505e04e4364939d3f503ed1044d
vancuong11 <cuongg121101@gmail.com>
2023-08-16 15:22:34 +0700

    #45 Xuất data ra file excel cutomize

--- src/components/TableComponent/TableComponent.js
+++ src/components/TableComponent/TableComponent.js
@@ -1,12 +1,14 @@
-import { Divider, Dropdown, Radio, Space, Table } from 'antd';
+import { Divider, Dropdown, Input, Modal, Radio, Space, Table } from 'antd';
 import { useRef, useState } from 'react';
 import Loading from '../LoadingComponent/Loading';
-import { DownloadTableExcel } from 'react-export-table-to-excel';
+import { DownloadTableExcel, useDownloadExcel } from 'react-export-table-to-excel';
 import './TableComponent.scss';
 
 function TableComponent(props) {
     const { selectionType = 'checkbox', data, isLoading, columns, handleDeleteMany } = props;
     const [rowSelectedKeys, setRowSelectedKeys] = useState([]);
+    const [fileName, setFileName] = useState('');
+
     const rowSelection = {
         onChange: (selectedRowKeys, selectedRows) => {
             setRowSelectedKeys(selectedRowKeys);
@@ -18,11 +20,33 @@ function TableComponent(props) {
         //     name: record.name,
         // }),
     };
+    const tableRef = useRef(null);
 
     const handleDeleteAll = () => {
         handleDeleteMany(rowSelectedKeys);
     };
-    const tableRef = useRef(null);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const showModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const handleCancel = () => {
+        setIsModalOpen(false);
+    };
+    const handleOnchange = (e) => {
+        setFileName(e.target.value);
+    };
+    const handleOk = () => {
+        onDownload();
+        setIsModalOpen(false);
+        setFileName('');
+    };
+    const { onDownload } = useDownloadExcel({
+        currentTableRef: tableRef.current,
+        filename: fileName,
+        sheet: 'Sheet',
+    });
+    console.log(fileName);
     return (
         <div>
             <Loading isLoading={isLoading}>
@@ -32,9 +56,10 @@ function TableComponent(props) {
                     </div>
                 )}
 
-                <DownloadTableExcel filename="Table Excel" sheet="Sheet" currentTableRef={tableRef.current}>
-                    <button> Export excel </button>
-                </DownloadTableExcel>
+                <button onClick={showModal}> Export excel </button>
+                <Modal title="Basic Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+                    <Input onChange={handleOnchange} placeholder="Lưu tên file excel"></Input>
+                </Modal>
 
                 <Table
                     ref={tableRef}
