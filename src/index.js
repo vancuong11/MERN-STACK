1cd8741a8e8effa46e65c546c1df9aa03b53f20f
vancuong11 <cuongg121101@gmail.com>
2023-08-19 16:50:54 +0700

    #54 tích hợp redux-persist với redux-toolkit

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { store } from './redux/store';
+import { persistor, store } from './redux/store';
 import { Provider } from 'react-redux';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const queryClient = new QueryClient();
 
@@ -15,7 +16,9 @@ root.render(
     // <React.StrictMode>
     <QueryClientProvider client={queryClient}>
         <Provider store={store}>
-            <App />
+            <PersistGate loading={null} persistor={persistor}>
+                <App />
+            </PersistGate>
         </Provider>
         <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>,
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,32 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import orderReducer from './slices/orderSlide';
 import userReducer from './slices/userSlide';
 import productReducer from './slices/productSlide';
-export const store = configureStore({
-    reducer: {
-        user: userReducer,
-        product: productReducer,
-        order: orderReducer,
-    },
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    blacklist: ['product', 'user'],
+};
+
+const rootReducer = combineReducers({
+    user: userReducer,
+    product: productReducer,
+    order: orderReducer,
 });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
+});
+
+export let persistor = persistStore(store);
